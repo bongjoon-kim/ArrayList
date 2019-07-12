e5791a44177399b01bbd2592748e21f828b51d49
Administrator <Administrator@system-kbj3>
2019-07-12 16:50:53 +0900

    강의안보고 get(), add(), allPrint() 구현

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -39,4 +39,46 @@ public class LinkedList {
 			size++;
 		}
 	}
+	
+	public void add(int index, Object input) {
+		Node newNode = new Node(input);
+		Node curNode = head;
+		Node nextNode = null;
+		if(size == 0 || index == 0) {
+			addFirst(input);
+		} else if(index >= size) {
+			addLast(input);
+		} else {
+			for (int i = 1; i < index; i++) {
+				nextNode = curNode.next;
+				curNode = nextNode;
+			}
+			newNode.next = curNode.next;
+			curNode.next = newNode;
+			size++;
+		}
+	}	
+	
+	public Object get(int index){
+		Node curNode = head;
+		Node nextNode = null;
+		if (index >= size) {
+			return -1;
+		}
+		for (int i = 0; i < index; i++) {
+			nextNode = curNode.next;
+			curNode = nextNode;
+		}
+		return curNode.toString();
+	}
+	
+	public void allPrint() {
+		System.out.print("[");
+		for(int i = 0; i < size; i++) {
+			System.out.print(get(i));
+			if (i < size-1)
+				System.out.print(",");
+		}
+		System.out.println("]");		
+	}
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -10,6 +10,17 @@ public class Main {
 		
 		numbers.addLast(40);
 		numbers.addLast(50);
+		numbers.addFirst(5);
+		
+		numbers.add(4,35);
+		numbers.add(1,7);
+		numbers.add(0,3);
+		numbers.add(9,60);
+		numbers.add(0,2);
+		numbers.add(6,25);
+		
+		System.out.println(numbers.get(5));
+		numbers.allPrint();
 	}
 
 }
